b12daf805842abca7c132bc9e8d3219a992bea81
ahmedsaheed <ahmedsaheed2@outlook.com>
2022-05-14 02:30:00 +0100

    New Look Version

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ export default class extends Component {
  <><head>
             <meta charset="UTF-8"></meta>
             <title>Ahmed Saheed</title>
-            <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.3.5/tailwind.min.css'/><link rel="stylesheet" href="./style.css"/>
-             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"></link>
-             <script src='https://cdn.jsdelivr.net/npm/theme-change@2.0.1/index.js'></script><script  src="./script.js"></script>
+            <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/1.3.5/tailwind.min.css'/><link rel='stylesheet' href="./style.css"/>
+             <link rel='stylesheet' href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css"></link>
+             <script src="https://cdn.jsdelivr.net/npm/theme-change@2.0.2/index.js"></script>
 
 
         </head>
@@ -20,7 +20,7 @@ export default class extends Component {
                                 <div class="social-network">
                                     <a href="https://github.com/ahmedsaheed" target="_blank"><i class="bi-github" aria-hidden="true"></i></a>
                                     <a href="https://twitter.com/helloahmed_" target="_blank"><i class="bi-twitter" aria-hidden="true"></i></a>
-                                    <a href="https://www.behance.net/ahmedsaheed" target="_blank"><i class="bi-book" aria-hidden="true"></i></a>
+                                    <a href="https://www.goodreads.com/user/show/139740691-ahmed-saheed" target="_blank"><i class="bi-book" aria-hidden="true"></i></a>
                                     
                                 </div>
                                 <div class="theme">
@@ -39,7 +39,7 @@ export default class extends Component {
                     <div class="back-titre">
                         <h1>Ahmed Saheed</h1>
                         <h2 class="tagline">Rather than love, than money, than fame, give me truth.</h2>
-                        <h2 class="tagline">Official family tech support since 7.</h2>
+                        <h2 class="tagline">Tolstoy's Fan & Official family tech support since 7.</h2>
 
                     </div>
                     <div class="feature-grid"><div class="feature"><a href="blog.html">
@@ -47,16 +47,13 @@ export default class extends Component {
                             life in the country, with the possibility of being useful to people to whom it is easy to do good, and who ar not accustomed to have it done to them; then work which one hope
                             may be of use; then rest, nature, books, music, love for ones neighbor - such is my ideal of happiness.
                         </p></a></div>
-                        <div class="feature"><a href="blog.html"><h3 class="feature-title">The Internet</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec at viverra ex, ut cursus ligula.Nullam fermentum molestie erat vitae tincidunt.Ut rutrum mi ac urna bibendum iaculis.In non sapien aliquet, semper dolor ut, volutpat felis.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Donec tempor in urna vel finibus.</p></a></div>
+                        <div class="feature"><a href="blog.html"><h3 class="feature-title">The Internet</h3><p>In science fiction which was published around the 1920s, "It is always expected that future human beings would have enormous heads because they would have very big brains and they would be very wise. It didn't work that ways, what happened is the human race is building a brain outside its body. That is to say an interlocking electronic network of telephonic, television, radionic communication which is rapidly intelocked with computers, so that you will within a few years be able to plug your brain into a computer".</p></a></div>
                         <div class="feature"><a href="blog.html"><h3 class="feature-title">End Of Everything</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at viverra ex, ut cursus ligula. Nullam fermentum molestie erat vitae tincidunt. Ut rutrum mi ac urna bibendum iaculis. In non sapien aliquet, semper dolor ut, volutpat felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor in urna vel finibus.</p></a></div>
                         <div class="feature"><a href="blog.html"><h3 class="feature-title">FLOW</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at viverra ex, ut cursus ligula. Nullam fermentum molestie erat vitae tincidunt. Ut rutrum mi ac urna bibendum iaculis. In non sapien aliquet, semper dolor ut, volutpat felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor in urna vel finibus.</p></a></div>
                         <div class="feature"><a href="blog.html"><h3 class="feature-title">Mastery Of Love</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at viverra ex, ut cursus ligula. Nullam fermentum molestie erat vitae tincidunt. Ut rutrum mi ac urna bibendum iaculis. In non sapien aliquet, semper dolor ut, volutpat felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor in urna vel finibus.</p></a></div>
                         <div class="feature"><a href="blog.html"><h3 class="feature-title">Believes, Cultures & Religion</h3><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at viverra ex, ut cursus ligula. Nullam fermentum molestie erat vitae tincidunt. Ut rutrum mi ac urna bibendum iaculis. In non sapien aliquet, semper dolor ut, volutpat felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor in urna vel finibus.</p></a></div></div>
-                
-                        
-
                 <br></br><br></br><br></br>
-                <footer class="footer"><div class="container"><div class="footer-inner"><center><p class="footer-text">Made with ❤︎ by the <a href="https://github.com/ahmedsaheed">Ahmed</a></p></center></div></div></footer>
+                <footer class="footer"><div class="container"><div class="footer-inner"><p class="footer-text">Made with ❤︎ by the <a href="https://github.com/ahmedsaheed">Ahmed</a></p></div></div></footer>
             </body></>
 
 
