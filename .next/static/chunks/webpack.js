e285b98718b19391d8e27ddc3dd141ed737a103f
ahmedsaheed <ahmedsaheed2@outlook.com>
2022-05-18 21:44:12 +0100

    removed unneeded css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f1110b504f9a8834"; }
+/******/ 		__webpack_require__.h = function() { return "f03b815b2e0d7ee7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
