b9b76cc427f6c97516fb0c9bc9408af74e723520
ahmedsaheed <ahmedsaheed2@outlook.com>
2022-05-19 13:02:34 +0100

    Trying to fix vercel map issues

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d1cc5db1bbfb84ec"; }
+/******/ 		__webpack_require__.h = function() { return "6724e9ac6fdd8355"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/BookList.js
+++ pages/BookList.js
@@ -8,7 +8,7 @@ const BookList = (props) => {
     return(
        <div>
             {
-                props && props.books.map((book, i) => {
+                props && props.books?.map((book, i) => {
                     return <section class="grid grid-cols-1 md:grid-cols-2 md:gap-28 xl:gap-56 space-y-28 md:space-y-0">
                     <BookCard
                     key={i}
