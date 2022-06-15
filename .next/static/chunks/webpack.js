e1dce38d2056c437b7a0742684850bfb3b64911b
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-06-15 18:57:34 +0100

    liitle twick

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "222d66d8c5d9e6e5"; }
+/******/ 		__webpack_require__.h = function() { return "6f68bfd3d9353346"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
