9383cf195737c417b503f87824353d22fce17990
Ahmed Saheed <87912847+ahmedsaheed@users.noreply.github.com>
2022-07-25 13:57:47 +0100

    Update summary.js

--- component/summary.js
+++ component/summary.js
@@ -6,11 +6,10 @@ const Summary = ({ title, content }) => {
             <h1 className="text-3xl font-am px-10">{title}</h1>
             <div
                 className="page-summary bg-am-black text-am-white mx-2 md:mx-0 p-10 rounded-lg prose max-w-full"
-                data-theme="business"
             >
                 <p
                     className="first-line:uppercase first-line:tracking-widest
-    first-letter:text-7xl first-letter:font-bold first-letter:text-white first-letter:mr-3 first-letter:float-left"
+    first-letter:text-7xl first-letter:font-bold first-letter:mr-3 first-letter:float-left"
                 >
                     {content}
                 </p>
