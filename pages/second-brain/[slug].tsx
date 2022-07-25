9761090759cda19f6548cc74a799ca0c281da236
Ahmed Saheed <87912847+ahmedsaheed@users.noreply.github.com>
2022-07-25 13:01:54 +0100

    Update [slug].tsx

--- pages/second-brain/[slug].tsx
+++ pages/second-brain/[slug].tsx
@@ -52,7 +52,7 @@ function BrainEntryPage({ brainEntry }: { brainEntry: BrainEntry }) {
                 </Head>
 
                 <header className=" vertical_dotted_line mx-auto max-w-3xl space-y-5">
-                    <div className="pads">
+                    <div className="padds">
                         <h1 className="post-title">{brainEntry.title}</h1>
                         <p className=" horizontal_dotted_line post-date">
                             {format(new Date(brainEntry.date), "MMM do, y")} |{" "}
