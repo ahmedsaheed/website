a9edfeb3e97aac2b0672e60549ebf8ff386e3558
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-06 14:28:17 +0100

    books and color

--- pages/books/BookComponent.tsx
+++ pages/books/BookComponent.tsx
@@ -4,20 +4,21 @@ import BookRating from "../../component/stars"
 const BookCard = (props: any) => {
     return (
         <div className="grid-item">
-            <h3 className="feature-title" />
-            <h3>{props.title}</h3>
-            <h3>{props.author}</h3>
-            <p>
-                Rating: <BookRating rating={props.rating} />
-            </p>
-
             <a href={props.url}>
                 <img
-                    style={{ display: "block", marginLeft: "auto", marginRight: "auto" }}
+                    style={{ display: "block", marginLeft: "auto", marginRight: "auto", borderRadius: "7px" }}
                     src={props.image}
                     alt="Books"
                 />
             </a>
+            <h3 style={{fontFamily: "Stylish"}}>{props.title}</h3>
+            <BookRating rating={props.rating} />
+            <h3>{props.author}</h3>
+
+              
+
+
+            
         </div>
     )
 }
