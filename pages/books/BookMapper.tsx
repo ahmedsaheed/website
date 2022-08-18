bffdf86b9e1386b8ad30d78439ea8ed3d7557903
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-08-18 23:41:05 +0100

    some font twicks @ index

--- pages/books/BookMapper.tsx
+++ pages/books/BookMapper.tsx
@@ -1,10 +1,7 @@
-import React, { Component } from "react"
+import React from "react"
 import BookCard from "./BookComponent"
 import Loader from "../../component/loader"
 const BookList = (props: any) => {
-    if (!props) {
-        return <Loader />
-    }
     return (
         <div>
             {props &&
