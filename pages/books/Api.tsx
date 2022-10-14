4d142d148a9a37b3646f6d2dc679f4e95cdc6990
Ahmed Saheed <ahmedsaheed2@outlook.com>
2022-10-14 21:50:14 +0100

    refactor book api to FC

--- pages/books/Api.tsx
+++ pages/books/Api.tsx
@@ -3,23 +3,24 @@ import request from "superagent"
 import BookList from "./BookMapper"
 import Loader from "../../component/loader"
 
-class Books extends Component {
-    state = {
-        books: [],
-        loading: true,
-    }
-    componentDidMount() {
-        this.getBooks()
-    }
-    getBooks = async () => {
-        const res = await request.get(
-            "https://www.googleapis.com/books/v1/users/101611817084658660916/bookshelves/4/volumes?&startIndex=0&maxResults=40&key=AIzaSyDNMnPGw3yUzfIwUnH3PR4oMJiP-PecbhU",
-        )
-        this.setState({ books: res.body.items, loading: false })
-    }
-    render() {
-        const { books, loading } = this.state
-        return <div className="bookpad">{loading ? <Loader /> : <BookList books={books} />}</div>
-    }
+
+
+export default function Books() {
+    const [books, setBooks] = React.useState([])
+    const [loading, setLoading] = React.useState(true)
+    
+    React.useEffect(() => {
+        request
+        .get("https://www.googleapis.com/books/v1/users/101611817084658660916/bookshelves/4/volumes?&startIndex=0&maxResults=40&key=AIzaSyDNMnPGw3yUzfIwUnH3PR4oMJiP-PecbhU")
+        .then((data) => {
+            setBooks(data.body.items)
+            setLoading(false)
+        })
+    }, [])
+    
+    return (
+        <div>
+        {loading ? <Loader /> : <BookList books={books} />}
+        </div>
+    )
 }
-export default Books
